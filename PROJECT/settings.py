7e9d1325965b2ed9dac6238edee5e9f30ee14637
mashood <mashoodurrehmanofficial@gmail.com>
2021-10-09 13:05:52 +0500

    first commit

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/Static/'
+STATIC_ROOT = '/home/PackerTracker/PackerTracker//Static2'
  
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Static')
