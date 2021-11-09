07a2bc7e473642275db7476fff67e3564d29630b
mashood <mashoodurrehmanofficial@gmail.com>
2021-11-09 15:48:50 +0500

    latest

--- Static/javascript/demo.js
+++ Static/javascript/demo.js
@@ -56,44 +56,46 @@ gradientBarChartConfiguration = {
         position: "nearest"
       },
       responsive: true,
-    //   animation: {
-    //     duration: 0,
-    //     easing: "easeOutQuart",
-    //     onComplete: function () {
-    //         var ctx = this.chart.ctx;
-    //         ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-    //         ctx.textAlign = 'center';
-    //         ctx.textBaseline = 'bottom';
+      animation: {
+        duration: 0,
+        easing: "easeOutQuart",
+        onComplete: function () {
+          alert(1)
+            var ctx = this.chart.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
 
-    //         this.data.datasets.forEach(function (dataset) {
-    //             for (var i = 0; i < dataset.data.length; i++) {
-    //                 var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-    //                     scale_max = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
-    //                 ctx.fillStyle = '#444';
-    //                 var y_pos = model.y - 5;
-    //                 // Make sure data value does not get overflown and hidden
-    //                 // when the bar's value is too close to max value of scale
-    //                 // Note: The y value is reverse, it counts from top down
-    //                 if ((scale_max - model.y) / scale_max >= 0.93)
-    //                     y_pos = model.y + 20; 
-    //                 ctx.fillText(dataset.data[i], model.x, y_pos);
-    //             }
-    //         });               
-    //     }
-    // },
-
-
-
-    animation: {
-      onComplete: function() {
-        this.chart.controller.draw();
-        drawValue(this, 1);
-      },
-      onProgress: function(state) {
-        var animation = state.animationObject;
-        drawValue(this, animation.currentStep / animation.numSteps);
-      }
+            this.data.datasets.forEach(function (dataset) {
+                for (var i = 0; i < dataset.data.length; i++) {
+                    var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
+                        scale_max = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._yScale.maxHeight;
+                    ctx.fillStyle = '#444';
+                    var y_pos = model.y - 5;
+                    // Make sure data value does not get overflown and hidden
+                    // when the bar's value is too close to max value of scale
+                    // Note: The y value is reverse, it counts from top down
+                    if ((scale_max - model.y) / scale_max >= 0.93)
+                        y_pos = model.y + 20; 
+                    ctx.fillText(dataset.data[i], model.x, y_pos);
+                    
+                }
+            });               
+        }
     },
+
+
+
+    // animation: {
+    //   onComplete: function() {
+    //     this.chart.controller.draw();
+    //     drawValue(this, 1);
+    //   },
+    //   onProgress: function(state) {
+    //     var animation = state.animationObject;
+    //     drawValue(this, animation.currentStep / animation.numSteps);
+    //   }
+    // },
 
 
 
